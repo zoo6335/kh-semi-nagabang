658d2f311188be125c81aaa33ab9b4cb8a0ac4a3
zoo <cute_duck@naver.com>
2022-10-24 17:28:43 +0900

    nagabang!!!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import NewsList from "../components/NewsList";
 import Categories from "../components/Categories";
 import { useCallback, useState } from "react";
-
+// 깃 연습
 const Home = () => {
     const [category, setCategory] = useState('all');
     const onSelect = useCallback(category => setCategory(category), []); // 화면 첫렌더링 시 카테고리값을 갖게 됨
